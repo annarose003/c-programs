a3e2f2dc81796808a2efc39caea337b4ccdc4f5e
annarose003 <63197078+annarose003@users.noreply.github.com>
2020-04-26 20:46:29 +0530

    Add files via upload

---
+++ test program.cpb.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter no;");
+scanf("%d",&n);
+printf("%d is the no",n);
+}
---
+++ testfile.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{int n;
+printf ("enter no");
+scanf ("%d",&n);
+printf ("number is %d",n);
+}
